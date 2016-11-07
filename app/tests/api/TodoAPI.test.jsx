469a1cbb32ed5059de09dd7a0dbbee54319345a9
Joe Coyle <joecoyle@me.com>
2016-11-07 18:53:19 +0000

    Added filter and search

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -40,4 +40,38 @@ describe ('TodoAPI', ()=>{
       expect(actual).toEqual([]);
     });
   });
+
+  describe('filterTodos', ()=>{
+    var todos = [{
+      id:1,
+      text:'test',
+      completed:true
+    },
+    {
+      id:2,
+      text:'test',
+      completed:false
+    },
+    {
+      id:3,
+      text:'test',
+      completed:true
+    }];
+
+    it('should return all todos', ()=> {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return filtered todos', ()=> {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it ('should sort by completed status', ()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+        expect(filteredTodos[0].completed).toBe(false);
+    });
+
+  });
 });
